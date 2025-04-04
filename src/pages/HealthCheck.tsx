a0cd8d69e4b5743c3a72c18ab8e5d440d7433f8d
kuattovak <108578467+kuatovakamila@users.noreply.github.com>
2025-04-04 22:27:41 +0500

    updated

--- src/pages/HealthCheck.tsx
+++ src/pages/HealthCheck.tsx
@@ -110,7 +110,6 @@ export default function HealthCheck() {
         onComplete={handleComplete}
     />
 )}
-
                     {currentState === "PULSE" && (
                         <LoadingCircle
                             icon={state.icon}
